d8be4067b0b444e99ea1e666aa8cafb666100d88
potatosim <devildriver2310@gmail.com>
2022-08-18 19:14:12 +0300

    feat: implement repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const {
+    repeatTimes,
+    separator,
+    addition,
+    additionRepeatTimes,
+    additionSeparator,
+  } = options;
+  const additionalString = Array(additionRepeatTimes)
+    .fill(addition === undefined ? '' : addition + '')
+    .join(additionSeparator || '|');
+  const mainString = Array(repeatTimes)
+    .fill(str + additionalString)
+    .join(separator || '+');
+  return mainString;
 }
+
+console.log(repeater('STRING', { repeatTimes: 3, separator: '**', 
+addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' }));
 
 module.exports = {
   repeater
