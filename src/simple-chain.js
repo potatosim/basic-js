a447efde103bec44fc0e1f7687a1ca41defc923a
potatosim <devildriver2310@gmail.com>
2022-08-21 17:18:24 +0300

    feat:implement chainMaker object

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,31 +2,42 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.chain.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (
+      position < 1 ||
+      position > this.getLength() ||
+      typeof position !== 'number'
+    ) {
+      this.chain = [];
+      throw new Error(`You can't remove incorrect link!`);
+    } else {
+      this.chain.splice(position - 1, 1);
+    }
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    const response = this.chain.join('~~');
+    this.chain = [];
+    return response;
+  },
 };
 
 module.exports = {
-  chainMaker
+  chainMaker,
 };
