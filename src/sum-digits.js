84383613b5c02da09dfc700150e034512be55764
potatosim <devildriver2310@gmail.com>
2022-08-22 15:02:56 +0300

    feat: implement getSumOfDigits function

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,11 +12,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  if (n < 10) {
+    return n;
+  }
+  const candidate = n
+    .toString()
+    .split('')
+    .reduce((acc, cur) => acc + +cur, 0);
+
+  return getSumOfDigits(candidate);
 }
 
 module.exports = {
-  getSumOfDigits
+  getSumOfDigits,
 };
