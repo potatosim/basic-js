9af6652d6d3b467fa77eb53929afd4b246be8002
potatosim <devildriver2310@gmail.com>
2022-08-23 19:12:58 +0300

    feat: implement Minesweeper game setup

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,11 +23,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const result = matrix.map(row => row.map(element => 0));
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i - 1]) {
+        if (matrix[i - 1][j - 1]) {
+          result[i][j]++;
+        }
+        if (matrix[i - 1][j]) {
+          result[i][j]++;
+        }
+        if (matrix[i - 1][j + 1]) {
+          result[i][j]++;
+        }
+      }
+      if (matrix[i + 1]) {
+        if (matrix[i + 1][j - 1]) {
+          result[i][j]++;
+        }
+        if (matrix[i + 1][j]) {
+          result[i][j]++;
+        }
+        if (matrix[i + 1][j + 1]) {
+          result[i][j]++;
+        }
+      }
+      if (matrix[i][j - 1]) {
+        result[i][j]++;
+      }
+      if (matrix[i][j + 1]) {
+        result[i][j]++;
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
